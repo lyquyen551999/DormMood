e990f52d07acd70e2a591891be168e4bde623e4b
lyquyen551999 <97889169+lyquyen551999@users.noreply.github.com>
2025-05-26 17:59:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 from auth_firebase import firebase_login, firebase_register
 import uuid
-import matchmaker  # Chỉ cần nếu gọi trong login hoặc preload
-import mood_journal  # Gợi ý nếu bạn đã chia file riêng
-import chat_match  # Nếu tách file riêng
 # Nếu không chia file riêng, thì viết từng phần trực tiếp (xem ví dụ dưới)
 
 # Cấu hình
