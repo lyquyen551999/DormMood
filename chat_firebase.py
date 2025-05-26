72fc51986cccac45040af06843146ce906648127
lyquyen551999 <97889169+lyquyen551999@users.noreply.github.com>
2025-05-26 23:00:47 +0800

    Update chat_firebase.py

--- chat_firebase.py
+++ chat_firebase.py
@@ -1,9 +1,6 @@
-
 import firebase_admin
 from firebase_admin import credentials, db
 from datetime import datetime
-import uuid
-import os
 import streamlit as st
 
 class ChatFirebase:
@@ -11,7 +8,15 @@ class ChatFirebase:
         if not firebase_admin._apps:
             cred = credentials.Certificate({
                 "type": st.secrets["firebase"]["type"],
-                ...
+                "project_id": st.secrets["firebase"]["project_id"],
+                "private_key_id": st.secrets["firebase"]["private_key_id"],
+                "private_key": st.secrets["firebase"]["private_key"],
+                "client_email": st.secrets["firebase"]["client_email"],
+                "client_id": st.secrets["firebase"]["client_id"],
+                "auth_uri": st.secrets["firebase"]["auth_uri"],
+                "token_uri": st.secrets["firebase"]["token_uri"],
+                "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+                "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
             })
             firebase_admin.initialize_app(cred, {
                 'databaseURL': st.secrets["firebase"]["databaseURL"]
